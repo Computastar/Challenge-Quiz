f34991953aaf6afa91b803761550992f0eaa8f00
Computastar <jason.hunter@computastar.com>
2023-01-17 17:56:04 +0000

    Added questions to index.js

--- starter/scripts/index.js
+++ starter/scripts/index.js
@@ -1,3 +1,15 @@
+const QUESTION_1 = new Question("Commonly used data types DO NOT include: ", 
+  ["Strings", "Booleans", "Alerts", "Numbers"], 2);
+const QUESTION_2 = new Question("The condition in an if / else statement is enclosed within ____.", 
+  ["Quotes", "Curly brackets", "Parentheses", "Square brackets"], 2);
+const QUESTION_3 = new Question("Arrays in JavaScript can be used to store ____.", 
+  ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"], 3);
+const QUESTION_4 = new Question("String values must be enclosed within _____ when being assigned to variables.", 
+  ["Commas", "Curly brackets", "Quotes", "Parentheses"], 2);
+const QUESTION_5 = new Question("A very useful tool used during development and debugging for printing content to the debugger is: ", 
+  ["JavaScript", "Terminal/Bash", "For Loops", "console.log"], 3);
+const QUESTION_LIST = [QUESTION_1, QUESTION_2, QUESTION_3, QUESTION_4, QUESTION_5];
+
 // A start button function
     // loads Q&A
     // starts timer
